696e000dbbf0863a6e708db1706792774d2c543a
MarkLewisII <mark.lewis070301@gmail.com>
2023-10-19 15:18:53 -0400

    Adds column of cells

--- scripts/script.js
+++ scripts/script.js
@@ -1,13 +1,17 @@
 function MakeBoard() {
-    const container = document.getElementById("container");
-    const cell = document.createElement("div");
+	const container = document.getElementById("container");
 
-    cell.setAttribute("class", "gridSquare");
-    cell.textContent = "test";
-    
-    for (let i = 0; i < 16; i++) {
-    for (let j = 0; j < 16; j++) {
-        container.setAttribute("class", "gridSquare");
-        }
-    }
+	for (let i = 0; i < 16; i++) {
+		const cell = document.createElement("div");
+		cell.setAttribute("class", "gridSquare");
+		for (let j = 0; j < 16; j++) {
+			const rows = document.createElement("div");
+			rows.setAttribute("class", "gridRow");
+			rows.appendChild(cell);
+			container.appendChild(rows);
+			// container.appendChild(cell);
+		}
+	}
 }
+
+MakeBoard();
