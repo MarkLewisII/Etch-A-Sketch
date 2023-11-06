4cb68cbae18a452ef3a173460b816f2997ba3f80
MarkLewisII <mark.lewis070301@gmail.com>
2023-11-06 16:14:37 -0400

    Adds 'Set size' button to the top of the screen

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,27 @@
-const container = document.getElementById("container");
-const cells = [];
-const rows = [];
+function Sketching() {
+	const container = document.getElementById("container");
+	const cells = [];
+	const rows = [];
 
-for (let i = 0; i < 16; i++) {
-	rows[i] = document.createElement("div");
-	rows[i].setAttribute("class", "gridColumn");
-	container.appendChild(rows[i]);
-	for (let j = 0; j < 16; j++) {
-		cells[j] = document.createElement("div");
-		cells[j].setAttribute("class", "gridCell");
-		rows[i].appendChild(cells[j]);
+	for (let i = 0; i < 16; i++) {
+		rows[i] = document.createElement("div");
+		rows[i].setAttribute("class", "gridColumn");
+		container.appendChild(rows[i]);
+		for (let j = 0; j < 16; j++) {
+			cells[j] = document.createElement("div");
+			cells[j].setAttribute("class", "gridCell");
+			rows[i].appendChild(cells[j]);
+		}
 	}
+
+	const squares = document.querySelectorAll(".gridCell");
+
+	squares.forEach((square) => {
+		square.addEventListener("mouseover", () => {
+			square.style.backgroundColor = "black";
+		});
+	});
 }
 
-const squares = document.querySelectorAll(".gridCell");
 
-squares.forEach((square) => {
-	square.addEventListener("mouseover", () => {
-		square.style.backgroundColor = "black";
-	});
-});
+Sketching();
